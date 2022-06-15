bba3ef1b4e8dd2a5b30343c1773422e9acc3a3ef
elyorxon <68835686+elyorxon@users.noreply.github.com>
2022-06-15 06:56:34 +0500

    added admin register

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    class Status:
+    class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
