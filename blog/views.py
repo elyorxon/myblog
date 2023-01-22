92cd0b49d8cec80c21cdc6583922a4d91688bc6a
elyorxon <68835686+elyorxon@users.noreply.github.com>
2023-01-22 06:22:41 +0500

    added inclusion tags

--- blog/views.py
+++ blog/views.py
@@ -41,7 +41,7 @@ def post_detail(request, year, month, day, post):
     form = CommentForm()
     # List of similar posts
     post_tags_ids = post.tags.values_list('id', flat=True)
-    similar_posts = Post.objects.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
     similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
     return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form,
                                                      'similar_posts': similar_posts})
